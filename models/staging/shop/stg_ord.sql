2ad65ab5ec3f99b3c34cf53909e76576c3408d86
sachinkalyan631@gmail.com <sachinkalyan631@gmail.com>
2024-10-05 20:25:21 +0000

    commit

--- models/staging/shop/stg_ord.sql
+++ models/staging/shop/stg_ord.sql
@@ -1,15 +1,12 @@
 {{ config (
    materialized ="table"
 )}}
-with order_table as (
-    select * from raw.shop.order_table
-),
- orders AS (
+ with orders AS (
     SELECT 
         id AS order_id,
         user_id AS customer_id,
         order_date,
         status
-    FROM order_table
+    FROM {{ source('shop', 'order_table') }}
     )
 select * from orders
